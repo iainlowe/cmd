7260407b89629429647e5c7c27162bbb8bd6539a
Iain Lowe <iain.lowe@gmail.com>
2015-05-19 21:46:25 -0400

    added test cases and a bunch of handling
    char* is correctly handled as a parameter and a return type
    arrays of strings handled as a parameter

---
+++ goch/test.h
@@ -0,0 +1,13 @@
+extern void ExternVoidVoid();
+void VoidVoid();
+int IntVoid();
+int IntInt(int a);
+char* StringVoid();
+char *StringVoid1();
+char* StringVoid2();
+char* StringString(char* b);
+char* StringString2(char *b);
+char* StringStringArray(char* b[]);
+float FloatVoid();
+// Comment test
+void CommentVoidVoid();
