24b4d886ae503ac559e57c49cc44222680444846
Iain Lowe <iain.lowe@gmail.com>
2015-06-24 13:06:13 -0400

    added find-dirty command

---
+++ find-dirty/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+var (
+	quiet = flag.Bool("q", false, "be quiet (just list dirs)")
+)
+
+func fullrel(s string) string {
+	s, _ = filepath.Abs(s)
+	wd, _ := os.Getwd()
+	return filepath.Join(".", strings.TrimPrefix(s, wd))
+
+}
+
+func findDirty(d string) {
+	d = fullrel(d)
+
+	if filepath.Base(d) == ".git" {
+		return
+	}
+
+	if _, err := os.Stat(filepath.Join(d, ".git")); err == nil {
+		c := exec.Command("git", "status", "-s")
+		c.Dir = d
+		b, err := c.CombinedOutput()
+
+		if err != nil {
+			fmt.Println("error:", d, err)
+			fmt.Println(string(b))
+		}
+
+		if strings.TrimSpace(string(b)) != "" {
+			fmt.Println(d)
+
+			if !*quiet {
+				fmt.Println(string(b))
+			}
+		}
+	}
+
+	infos, _ := ioutil.ReadDir(d)
+
+	for _, info := range infos {
+		if info.IsDir() {
+			findDirty(filepath.Join(d, info.Name()))
+		}
+	}
+}
+
+func main() {
+	flag.Parse()
+	findDirty(".")
+}
